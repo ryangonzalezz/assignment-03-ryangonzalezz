13e2572c05e601199b1584c75fc044c28f6e6d11
Ryan Gonzalez <ryangonzalez@cmail.carleton.ca>
2023-03-29 00:15:49 -0400

    Adding items

--- main.js
+++ main.js
@@ -7,3 +7,62 @@
 // Variables
 //
 
+const reminders = [];
+const reminderList = document.querySelector(".reminder-list");
+const reminderForm = document.querySelector(".add-reminder");
+const reminderName = document.querySelector("#new-reminder-input");
+
+//
+// Functions
+//
+
+function addReminderItem(e) 
+{
+  
+  e.preventDefault();
+  const rName = reminderName.value;
+   const rNull = rName === '';
+if (rNull) 
+{
+     alert("Please write a reminder")
+}
+else 
+{  
+ reminders.push(rName);
+  renderList(reminders, reminderList);
+  reminderForm.reset();
+}
+}
+
+function renderList(items, itemsList) 
+{
+ 
+  while (itemsList.firstChild) 
+  {
+    itemsList.removeChild(itemsList.firstChild);
+  }
+ 
+  for (let i = 0; i < items.length; i++)
+  {
+    const listItem = document.createElement("li");
+    listItem.textContent = items[i];
+
+    const rDelete = document.createElement('button');
+    rDelete.innerHTML = "<img src='images/Trash.png' width= 25px>";
+    listItem.appendChild(rDelete);
+
+    rDelete.style.cursor = "pointer";
+
+   if (i === items.length - 1) 
+    {
+      listItem.classList.add("new-item-annimate");
+    }
+    itemsList.appendChild(listItem);
+  }
+}
+
+reminderForm.addEventListener("submit", addReminderItem);
+
+renderList(reminders, reminderList);
+
+
